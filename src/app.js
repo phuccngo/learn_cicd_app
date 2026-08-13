29a5f752c5b517b76980a24ee63bdeb12b9ec661
Ngô Thanh Phúc <ntphuc2202@gmail.com>
2026-08-13 15:01:12 +0700

    change app.js and add server.js

--- src/app.js
+++ src/app.js
@@ -23,8 +23,10 @@ res.status(500).json({ error: 'Something went wrong!' });
 });
 
 // Start server
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
